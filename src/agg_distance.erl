a799afc9fd160311c42b5873557a02f6cd63fcd7
Vladimir Goncharov <dev@viruzzz.org>
2016-06-02 16:05:26 +0300

    Skip invalid points

--- src/agg_distance.erl
+++ src/agg_distance.erl
@@ -5,43 +5,47 @@
 process(Data,{_ExtInfo,_PrevAggregated},_Prev,_Config) ->
 	Fx = fun(Elm,{Min,Max,MaxS,PPos,PathSum}) ->
 				 %[{dt,1420722010},{position,[37.59247079869102,51.620521248073814]},{dir,266.4217921976619},{sp,0},{v_fuel,25.10293735686435},{v_odometer,9308850.076139404}]
-				 Odometer=proplists:get_value(v_odometer,Elm),
-				 Speed=proplists:get_value(sp,Elm),
-				 Pos=proplists:get_value(position,Elm),
-				 MaxS1 = case {Speed, MaxS} of 
-							 {undefined, _} -> MaxS;
-							 {_, undefined} -> Speed;
-							 _ when Speed > MaxS -> Speed;
-							 _ -> MaxS
-						 end,
-				 {Min1,Max1} = case Odometer of 
-								   undefined -> {Min,Max};
-								   _ ->
-									   {case Min of
-											undefined -> Odometer;
-											X when Odometer < X -> Odometer;
-											_ -> Min
-										end,
-										case Max of
-											undefined -> Odometer;
-											X when Odometer > X -> Odometer;
-											_ -> Max
-										end}
-							   end,
-				 Dist=case Pos of 
-						  PPos -> 0;
-						  [Lon,Lat] -> case PPos of 
-										   [Lon1,Lat1] ->
-											   {_Az,XDist} = gpstools:sphere_inverse({Lon,Lat},{Lon1,Lat1}),
-											   XDist;
-										   _ -> 0
-									   end;
-						  _ -> 0
-					  end,
+				 case proplists:get_value(valid,Elm,true) of
+						   false -> 
+							   {Min,Max,MaxS,PPos,PathSum};
+						   _ -> 
+							   Odometer=proplists:get_value(v_odometer,Elm),
+							   Speed=proplists:get_value(sp,Elm),
+							   Pos=proplists:get_value(position,Elm),
+							   MaxS1 = case {Speed, MaxS} of 
+										   {undefined, _} -> MaxS;
+										   {_, undefined} -> Speed;
+										   _ when Speed > MaxS -> Speed;
+										   _ -> MaxS
+									   end,
+							   {Min1,Max1} = case Odometer of 
+												 undefined -> {Min,Max};
+												 _ ->
+													 {case Min of
+														  undefined -> Odometer;
+														  X when Odometer < X -> Odometer;
+														  _ -> Min
+													  end,
+													  case Max of
+														  undefined -> Odometer;
+														  X when Odometer > X -> Odometer;
+														  _ -> Max
+													  end}
+											 end,
+							   Dist=case Pos of 
+										PPos -> 0;
+										[Lon,Lat] -> case PPos of 
+														 [Lon1,Lat1] ->
+															 {_Az,XDist} = gpstools:sphere_inverse({Lon,Lat},{Lon1,Lat1}),
+															 XDist;
+														 _ -> 0
+													 end;
+										_ -> 0
+									end,
 
-			{Min1,Max1,MaxS1,Pos,PathSum+Dist}
+							   {Min1,Max1,MaxS1,Pos,PathSum+Dist}
+					   end
 		 end,
-	{MSec,Sec,_} = now(),
 	{OMin0, OMax, MaxS, _, PathLen}=lists:foldl(Fx, {undefinded, 0, 0, undefined, 0}, Data),
 	OMin=case OMin0 of
 			 Num when is_integer(Num) -> Num;
@@ -56,7 +60,7 @@ process(Data,{_ExtInfo,_PrevAggregated},_Prev,_Config) ->
 		  {<<"sum_v_odometer">>,OMax-OMin}, 
 		  {<<"pathlen">>, PathLen*1000}, 
 		  {<<"max_speed">>, MaxS},
-		  {<<"t">>, MSec*1000000 + Sec}
+		  {<<"t">>, os:system_time(seconds) }
 		 ]}.
 
 
